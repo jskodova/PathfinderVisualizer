872673eba32e4f01f71061a5d7ad5185cdbae8d3
jskodova <skodovajarmilatt@gmail.com>
2023-07-04 00:38:05 +0300

    A star algorithm

---
+++ src/algorithms/astar.js
@@ -0,0 +1,84 @@
+export function astar(grid, startNode, finishNode) {
+    const openSet = [];
+    const closedSet = [];
+    startNode.gScore = 0;
+    startNode.fScore = heuristic(startNode, finishNode);
+    openSet.push(startNode);
+
+    while (openSet.length > 0) {
+      const currentNode = getLowestFScoreNode(openSet);
+      if (currentNode === finishNode) {
+        return reconstructPath(currentNode);
+      }
+
+      removeFromArray(openSet, currentNode);
+      closedSet.push(currentNode);
+
+      const neighbors = getNeighbors(currentNode, grid);
+      for (const neighbor of neighbors) {
+        if (closedSet.includes(neighbor) || neighbor.isWall) {
+          continue;
+        }
+
+        const tentativeGScore = currentNode.gScore + 1;
+        if (!openSet.includes(neighbor)) {
+          openSet.push(neighbor);
+        } else if (tentativeGScore >= neighbor.gScore) {
+          continue;
+        }
+
+        neighbor.previousNode = currentNode;
+        neighbor.gScore = tentativeGScore;
+        neighbor.fScore = neighbor.gScore + heuristic(neighbor, finishNode);
+      }
+    }
+
+    return []; // No path found
+  }
+
+  function heuristic(nodeA, nodeB) {
+    const dx = Math.abs(nodeA.col - nodeB.col);
+    const dy = Math.abs(nodeA.row - nodeB.row);
+    return dx + dy;
+  }
+
+  function getLowestFScoreNode(nodes) {
+    let lowestNode = nodes[0];
+    for (let i = 1; i < nodes.length; i++) {
+      if (nodes[i].fScore < lowestNode.fScore) {
+        lowestNode = nodes[i];
+      }
+    }
+    return lowestNode;
+  }
+
+  function removeFromArray(array, element) {
+    const index = array.indexOf(element);
+    if (index !== -1) {
+      array.splice(index, 1);
+    }
+  }
+
+  function getNeighbors(node, grid) {
+    const neighbors = [];
+    const { col, row } = node;
+
+    if (row > 0) neighbors.push(grid[row - 1][col]);
+    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+    if (col > 0) neighbors.push(grid[row][col - 1]);
+    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+
+    return neighbors;
+  }
+
+  function reconstructPath(node) {
+    const path = [];
+    let currentNode = node;
+
+    while (currentNode !== null) {
+      path.unshift(currentNode);
+      currentNode = currentNode.previousNode;
+    }
+
+    return path;
+  }
